2a7e0fe8efcdcdb67f014632d7705b9893ff0c13
Oisin Quinn <oisin1001@gmail.com>
2018-08-02 21:38:12 +0100

    Set up database URL

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'oisin',
-      password : '',
-      database : 'smart-brain'
+      connectionString: process.env.DATABASE,
+        ssl: true
     }
 });
 
